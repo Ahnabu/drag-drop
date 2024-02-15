07c6d06de281b773b3f5dfe09a082943f0f067e9
Ahnabu <horairaabu013025@gmail.com>
2024-02-15 20:43:58 +0600

    finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-let container=document.getElementById('container')
+let container = document.getElementById('container');
+let ball = document.getElementById('ball');
+
+
+let initialTop, initialLeft;
+function ballHandler(e) {
+    let top = e.clientY - initialTop;
+    let left = e.clientX - initialLeft;
+    ball.style.top = `${top}px`;
+    ball.style.left = `${left}px`;
+    console.log('clicked')
+}
+ball.addEventListener('mousedown', function (e) {
+    if (!initialTop && !initialLeft) {
+        initialTop = e.clientY;
+        initialLeft = e.clientX;
+    }
+    container.addEventListener('mouseover',ballHandler)
+})
+ball.addEventListener('mouseup', function (e) {
+    container.removeEventListener('mouseover', ballHandler)
+})
